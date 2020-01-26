1754c0c71ec10b0c1d6feffb73becc310b6901c9
matt <mholford@protonmail.com>
2020-01-26 12:47:23 -0800

    Initial commit

---
+++ app/src/main/java/android/bignerdranch/com/NewCharacterActivity.kt
@@ -0,0 +1,45 @@
+package android.bignerdranch.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_new_character.*
+
+private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
+
+private var Bundle.characterData
+get() = getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
+set(value) = putSerializable(CHARACTER_DATA_KEY, value)
+
+class NewCharacterActivity : AppCompatActivity() {
+    private var characterData = CharacterGenerator.generate()
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.characterData = characterData
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_character)
+
+        characterData = savedInstanceState?.characterData ?:
+                CharacterGenerator.generate()
+
+        generateButton.setOnClickListener {
+            characterData = CharacterGenerator.generate()
+            displayCharacterData()
+        }
+
+        displayCharacterData()
+    }
+
+    private fun displayCharacterData() {
+        characterData.run {
+            nameTextView.text = name
+            raceTextView.text = race
+            dexterityTextView.text = dex
+            wisdomTextView.text = wis
+            strengthTextView.text = str
+        }
+    }
+}
